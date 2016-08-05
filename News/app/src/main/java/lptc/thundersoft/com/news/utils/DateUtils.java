0b57b2c0dbdcc8965c282fa503832384c04ce19f
tn2 <tn2@thundersoft.com>
2016-08-05 16:13:38 +0800

    net success

--- News/app/src/main/java/lptc/thundersoft/com/news/utils/DateUtils.java
+++ News/app/src/main/java/lptc/thundersoft/com/news/utils/DateUtils.java
@@ -2,178 +2,48 @@ package lptc.thundersoft.com.news.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 /**
  * 常用时间工具类
  */
-public class DateUtils
-{
+public class DateUtils {
+    public static final String GankDateFormate = "yyyy-MM-ddThh:mm:ss";
 
-    public static SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
 
-    public static SimpleDateFormat formatDay = new SimpleDateFormat("d", Locale.getDefault());
+    public static String changeDateFormate(String srcString) {
+        String str = srcString.split(".")[0];
+        long timeMillion;
+        try {
+            Date srcDate = SimpleDateFormat.getDateInstance().parse(GankDateFormate);
+            timeMillion = srcDate.getTime();
+            long timeCur = System.currentTimeMillis();
+            long index = timeCur - timeMillion;
+            long target = timeCur % (24 * 60 * 60 * 1000);
 
-    public static SimpleDateFormat formatMonthDay = new SimpleDateFormat("M-d", Locale.getDefault());
+            //同一天
+            if (index <= target) {
+                long hour = index / (60 * 60 * 1000);
+                return hour + "小时前";
+            }
+            //昨天
+            else if (index <= (24 * 60 * 60 * 1000 + target)) {
+                return "昨天";
+            }
+            //前天
+            else if (index <= (48 * 60 * 60 * 1000 + target)) {
+                return "前天";
+            }
+            //显示日期
+            else {
+                return str.split("T")[0].replaceAll("-", "\\\n");
+            }
 
-    public static SimpleDateFormat formatDateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
 
-    /**
-     * 格式化日期
-     *
-     * @param date
-     * @return 年月日
-     */
-    public static String formatDate(Date date)
-    {
-
-        return formatDate.format(date);
-    }
-
-    /**
-     * 格式化日期
-     *
-     * @param date
-     * @return 年月日 时分秒
-     */
-    public static String formatDateTime(Date date)
-    {
-
-        return formatDateTime.format(date);
-    }
-
-    /**
-     * 将时间戳解析成日期
-     *
-     * @param timeInMillis
-     * @return 年月日
-     */
-    public static String parseDate(long timeInMillis)
-    {
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(timeInMillis);
-        Date date = calendar.getTime();
-        return formatDate(date);
-    }
-
-    /**
-     * 将时间戳解析成日期
-     *
-     * @param timeInMillis
-     * @return 年月日 时分秒
-     */
-    public static String parseDateTime(long timeInMillis)
-    {
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(timeInMillis);
-        Date date = calendar.getTime();
-        return formatDateTime(date);
-    }
-
-    /**
-     * 解析日期
-     *
-     * @param date
-     * @return
-     */
-    public static Date parseDate(String date)
-    {
-
-        Date mDate = null;
-        try
-        {
-            mDate = formatDate.parse(date);
-        } catch (ParseException e)
-        {
+        } catch (ParseException e) {
             e.printStackTrace();
+            return "parse error";
         }
-
-        return mDate;
     }
 
-    /**
-     * 解析日期
-     *
-     * @param datetime
-     * @return
-     */
-    public static Date parseDateTime(String datetime)
-    {
-
-        Date mDate = null;
-        try
-        {
-            mDate = formatDateTime.parse(datetime);
-        } catch (ParseException e)
-        {
-            e.printStackTrace();
-        }
-
-        return mDate;
-    }
-
-
-    /**
-     * 以友好的方式显示时间
-     *
-     * @param sdate
-     * @return
-     */
-    public static String friendlyTime(String sdate)
-    {
-
-        Date time = parseDateTime(sdate);
-        if (time == null)
-        {
-            return "Unknown";
-        }
-        String ftime = "";
-        Calendar cal = Calendar.getInstance();
-
-        // 判断是否是同一天
-        String curDate = formatDate.format(cal.getTime());
-        String paramDate = formatDate.format(time);
-        if (curDate.equals(paramDate))
-        {
-            int hour = (int) ((cal.getTimeInMillis() - time.getTime()) / 3600000);
-            if (hour == 0)
-                ftime = Math.max(
-                        (cal.getTimeInMillis() - time.getTime()) / 60000, 1)
-                        + "分钟前";
-            else
-                ftime = hour + "小时前";
-            return ftime;
-        }
-
-        long lt = time.getTime() / 86400000;
-        long ct = cal.getTimeInMillis() / 86400000;
-        int days = (int) (ct - lt);
-        if (days == 0)
-        {
-            int hour = (int) ((cal.getTimeInMillis() - time.getTime()) / 3600000);
-            if (hour == 0)
-                ftime = Math.max(
-                        (cal.getTimeInMillis() - time.getTime()) / 60000, 1)
-                        + "分钟前";
-            else
-                ftime = hour + "小时前";
-        } else if (days == 1)
-        {
-            ftime = "昨天";
-        } else if (days == 2)
-        {
-            ftime = "前天";
-        } else if (days > 2 && days <= 10)
-        {
-            ftime = days + "天前";
-        } else if (days > 10)
-        {
-            ftime = formatDate.format(time);
-        }
-        return ftime;
-    }
 }
